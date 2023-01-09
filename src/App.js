93e33a89237323dec33aa499985adf606d97f36c
Erfan Rezaei <erfan.rezaei2883@gmail.com>
2023-01-09 15:40:27 +0330

    show list Todo in file Todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import AddTodo from "./Components/AddTodo";
 import { useSelector } from "react-redux";
+import Todo from "./Components/Todo";
 function App() {
 
-  const toods = useSelector(state=>state.TodoList.list)
-  console.log(toods)
+  const todos = useSelector(state=>state.TodoList.list)
+  
 
   return (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
@@ -13,18 +14,17 @@ function App() {
              <AddTodo/>
           </div>
           <div>
-              <div className="flex mb-4 items-center">
-                  <p className="mr-auto text-gray-700">Add another component to Tailwind Components</p>
-                  <button className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600">Done</button>
-                  <button className="p-1 px-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600">Remove</button>
-              </div>
-              <div className="flex justify-between mb-4 items-center">
+              {
+                !todos.length ? <p style={{color:"red"}} >No Items...</p> :
+                todos.map(todo =><Todo key={todo.id} item={todo}/>)
+              }
+              {/* <div className="flex justify-between mb-4 items-center">
                 <p className="mr-auto line-through text-green-600">Submit Todo App Component to Tailwind Components</p>
                 <div className="flex items-center">
                   <button className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-400 border-gray-400 hover:bg-gray-400">Not Done</button>
                   <button className="p-1 px-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600">Remove</button>
                 </div>  
-              </div>
+              </div> */}
           </div>
       </div>
   </div>
