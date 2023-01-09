857c6a1097ae05e55b0edaf731822f2b13038ece
Erfan Rezaei <erfan.rezaei2883@gmail.com>
2023-01-09 16:20:10 +0330

    added action EditTodo in project

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import { deleteTodo } from '../Store/Slice/TodoList';
+import { useDispatch } from 'react-redux';
+import { editTodo } from '../Store/Slice/TodoList';
 function Todo({item}) {
+    const dispatch =useDispatch();
+    const EditHandler=()=>{
+        dispatch(editTodo({id:item.id}))
+    }
     return (
         <>
              <div className="flex mb-4 items-center">
-                  <p className="mr-auto text-gray-700">{item.text}</p>
-                  <button className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600">Done</button>
-                  <button className="p-1 px-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600">Remove</button>
+                  <p className={`mr-auto  ${!item.done ? "text-gray-700" : "line-through text-green-600"}`}>{item.text}</p>
+
+                  {
+                    item.done ?          
+                    <button onClick={EditHandler} className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-gray-400 border-gray-400 hover:bg-gray-400">Not Done</button>
+                    :
+                    <button onClick={EditHandler} className="p-1 px-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-600 border-green-600 hover:bg-green-600">Done</button>
+                    }
+
+                  <button onClick={()=>dispatch(deleteTodo(item.id))} className="p-1 px-2 ml-2 border-2 rounded text-red-600 border-red-600 hover:text-white hover:bg-red-600">Remove</button>
               </div>
         </>
     );
--- src/Store/Slice/TodoList.js
+++ src/Store/Slice/TodoList.js
@@ -9,10 +9,28 @@ const TodoList = createSlice({
     reducers:{
         add : (state,action)=>{
                 state.list.push(action.payload)
+        },
+
+        deleteTodo:(state,action)=>{
+            state.list=state.list.filter(item=> item.id != action.payload)
+        },
+
+        editTodo:(state,action)=>{
+            state.list=state.list.map(item=>{
+                return item.id ==action.payload.id
+                ?
+                
+                {
+                    ...item,
+                    done:!item.done
+                }
+                :
+                item
+            })
         }
     }
 })
 
-export const {add} = TodoList.actions
+export const {add,deleteTodo,editTodo} = TodoList.actions
 
 export default TodoList.reducer;
